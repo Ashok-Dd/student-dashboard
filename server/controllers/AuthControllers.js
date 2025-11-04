b3da94ffb48017a1064be0e89903037b22a26f8a
Ashok-Dd <bonguashok86@gmail.com>
2025-11-04 23:03:06 +0530

    added sameSite for logout

--- server/controllers/AuthControllers.js
+++ server/controllers/AuthControllers.js
@@ -220,7 +220,7 @@ export const logout = async(req , res) => {
     try {
         res.clearCookie("authToken" , {
             httponly: true ,
-            sameSite:"strict",
+            sameSite:"None",
             secure : process.env.NODE_ENV === "production" ,
         })
         return res.status(200).json({
